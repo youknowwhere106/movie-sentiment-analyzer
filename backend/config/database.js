aadaed843bf063f624940a21841fad321cd8f0ad
Piyush Awasthi <piyushawasthi106@gmail.com>
2025-06-26 17:10:46 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,10 @@
  const { Pool } = require('pg');
 
 const pool = new Pool({
-   connectionString: process.env.DATABASE_URL || process.env.POSTGRES_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+   connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
