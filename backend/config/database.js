4d40ca5d41650304ac214c0199334dbb85376944
Piyush Awasthi <piyushawasthi106@gmail.com>
2025-06-26 17:22:40 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,11 @@
  const { Pool } = require('pg');
 
 const pool = new Pool({
-   connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  user: process.env.DB_USER || ,
+  host: process.env.DB_HOST || ,
+  database: process.env.DB_NAME || ,
+  password: process.env.DB_PASSWORD || ,
+  port: process.env.DB_PORT || ,
 });
 
 const initDatabase = async () => {
